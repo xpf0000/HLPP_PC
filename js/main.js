99c7b24a8b2ee42572d8b4b1cef0e7bfda40bef6
xpf0000 <xpf_163@163.com>
2016-07-21 18:24:59 +0800

    +++

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*
+require.config({
+	
+	baseUrl : "js",
+	
+    paths : {
+        "jquery" : "jquery-1.8.0.min",
+        "avalon" : "avalon",
+        "SuperSlide" : "jquery.SuperSlide.2.1.1",
+        "gotop" : "gotop",
+        "foucs" : "foucs"   
+    }
+})
+*/
+
+
+var vm = avalon.define({
+                $id: "test",
+                name: "司徒正美",
+                array: [11,22,33]
+        })
+
+
+/*
+requirejs(['avalon'],function(avalon){
+	
+		var vm = avalon.define({
+                $id: "test",
+                name: "司徒正美",
+                array: [11,22,33]
+        })
+        
+        
+        
+	
+	
+	
+})
+*/
